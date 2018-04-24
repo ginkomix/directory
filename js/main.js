c507b256342ea3c64d801afdb1a96200579cc44f
Гинько Михаил <accer96226@gmail.com>
2018-04-24 17:38:27 +0300

    menu algoritm add

--- js/main.js
+++ js/main.js
@@ -1,29 +1,42 @@
 class Main {
-    constructor() {
-        this.goods = [];
-        this.goodGroups = [];
-        this.importInformation();
-    }
+  constructor() {
+    this.goods = [];
+    this.goodGroups = [];
+    this.goodGroupss = [];
+    this.importInformation();
+    this.start();
+  }
+
+  start() {
+    document.querySelector("#menu").addEventListener("click", menu.changeMenu);
+  }
+
+  importInformation() {
+    api
+      .getInformation("goods")
+      .then(json => json.json())
+      .then(information => {
+        this.goods = information;
+      });
+
+    api
+      .getInformation("goodGroups")
+      .then(json => json.json())
+      .then(information => {
+        this.goodGroups = information;
+      });
+  }
+
+  wait(what, call) {
+    console.log("wait");
     
-    importInformation(){
-        api
-          .getInformation('goods')
-          .then(json => json.json())
-          .then(information => {
-           this.goods = information;
-          }); 
-
-          api
-            .getInformation("goodGroups")
-            .then(json => json.json())
-            .then(information => {
-              this.goodGroups = information;
-            });         
+    if (what.length === 0) {
+      
+      setTimeout(() => call(), 100);
+      return false;
     }
-
-
-
-
+    return true;
+  }
 }
 
 let main = new Main();
--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,28 @@
 class Menu {
-    constructor(){
-        this.chainMenu =[2000];
+  constructor() {
+    this.chainMenu = [2000];
+    this.changeMenu = this.changeMenu.bind(this);
+  }
+  changeMenu(ev) {
+    let classListMenu = ev.target.classList;
+    if (!classListMenu.contains("item-menu")) {
+      return;
     }
-    
-}
+    this.chainMenu[this.chainMenu.length - 1] === +classListMenu[1]
+      ? this.addItemMenu(+classListMenu[1])
+      : this.removeItemMenu(+classListMenu[1]);
+  }
+  addItemMenu() {
+      if(!main.wait(main.goodGroups,menu.addItemMenu)){
+        return;
+      }
+    this.chainMenu.push(";");
+  }
 
+  removeItemMenu(item) {
+    let length = this.chainMenu.length,
+      indexItem = this.chainMenu.indexOf(item) + 1;
+    this.chainMenu.splice(indexItem, length);
+  }
+}
 let menu = new Menu();
